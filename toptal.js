f8b49ff330fe880b0b528dd8fec9ebccb974993b
capitalch <capitalch@gmail.com>
2019-01-04 20:47:40 +0530

    init

--- toptal.js
+++ toptal.js
@@ -1,31 +1,21 @@
 'use strict'
-let cnt = 0;
-let lastCarry = 0;
-function solution(a, b) {
-    let as = a.toString();
-    let bs = b.toString();
-    let aArr = as.split('').reverse();
-    let bArr = bs.split('').reverse();
-    let lenA = aArr.length - 1;
-    let lenB = bArr.length - 1;
-    const len = Math.max(lenA, lenB);
-    for (let i = 0; i <= len; i++) {
-        let valA = parseInt(aArr[i]) || 0;
-        let valB = parseInt(bArr[i]) || 0;
-        let val = valA + valB + lastCarry;
-        if (val > 9) {
-            cnt++;
-            lastCarry = 1;
-        } else {
-            lastCarry = 0;
-        }
+function solution(id) {
+    if (id.length == 1) {
+        return parseInt(id[0]);
     }
-    return (cnt);
+    const arr = id.split('');
+    const val = arr.reduce((p, v) => { return (p + parseInt(v)) }, 0)
+    if (val <= 9) {
+        return (val)
+    } else {
+        solution(val.toString());
+    }
 }
-const a = 19, b = 92;
 
-const ret = solution(a, b);
+const id = '5'
+const ret = solution(id);
 console.log(ret);
+
 module.exports = {
     solution: solution
 }
